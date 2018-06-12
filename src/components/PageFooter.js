5b1e21ec240172b3504b31cec8838adef8726edf
Babubhai <shubhsharma10@gmail.com>
2018-06-12 17:39:27 -0400

    Added github repo link in footer

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,7 +5,10 @@ export default class PageFooter extends React.Component {
         return(
             <nav className="navbar navbar-light bg-light fixed-bottom justify-content-end">
                 <div className="navbar-text">
-                    <h6>Developed by Shubham Sharma © 2018</h6>
+                    <h6><a className="mr-2" href="https://github.com/shubhsharma10/Lyricpedia/">
+                            <i className="fa fa-github"/>
+                        </a>
+                        Developed by Shubham Sharma © 2018</h6>
                 </div>
             </nav>
         );
